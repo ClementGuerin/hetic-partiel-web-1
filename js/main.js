9778ad27a76999308f65a1d53299f6d7cdd640dc
ClementGuerin <ticlem27270@gmail.com>
2018-06-14 11:02:40 +0200

    Fixes #1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Mobile Burger Menu
+
+var btnMobileMenu = document.querySelector('#menu_mobile');
+var mobileMenuState = false;
+
+btnMobileMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    if(mobileMenuState === false){
+        document.querySelector('#mainav').style.display = "block";
+        mobileMenuState = true;
+    } else {
+        document.querySelector('#mainav').style.display = "";
+        mobileMenuState = false;
+    }
+}
